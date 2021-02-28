a7c0f3332d566491ac4c65ad8285b5319426cbea
Ilya Lyaukin <kzerby@protonmail.com>
2021-02-28 19:57:10 +0300

    Implemented some builders

--- mongomoron/__init__.py
+++ mongomoron/__init__.py
@@ -1,1 +1,4 @@
-from .mongomoron import DatabaseConnection, index
+from .mongomoron import DatabaseConnection, Collection, index, insert_one, \
+    insert_many, update, update_one, delete, query, query_one, and_, not_, nor_, \
+    or_, aggregate, sum_, min_, max_, avg, push_, accumulator, unique_values, \
+    first, last, filter_, cmp, dict_, list_, document
